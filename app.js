a669e84d9f443c7a54228ee1ec5c399cc7e8bac3
Sumair-Ahmed <ahmednahiyo21@gmail.com>
2024-12-22 16:01:55 +0500

    New changes

--- app.js
+++ app.js
@@ -5,21 +5,24 @@ const productRoutes = require("./Routes/productRoute");
 const articleRoutes = require("./Routes/articleRoute");
 const cors = require("cors");
 const path = require("path");
+require("dotenv").config();
+
 const app = express();
 const port = process.env.PORT || 3000;
+const DBHOST = process.env.DBHOST;
 
 // Connect to MongoDB
 mongoose
-  .connect(
-    "mongodb+srv://ahmednahiyo21:8auxqjaAIg1m3z4w@backenddb.qclzc.mongodb.net/Learning-API?retryWrites=true&w=majority&appName=BackendDB"
-  )
-  // Replace with your MongoDB connection string
+  .connect(DBHOST)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
 // Middleware
-app.use(cors()); // Optional, for CORS configuration
+app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Project Running Properly");
+});
 app.use("/api/dogs", dogRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/articles", articleRoutes);
